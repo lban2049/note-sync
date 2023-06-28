c8d0ec16819b49bc9d8952b34a85acbb447ebece
LBan <hanxl5123@163.com>
2023-06-28 10:49:53 +0800

    init

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,26 @@
 import { useState } from "react"
+import './index.css'
+import * as Tabs from '@radix-ui/react-tabs';
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
     <div
+      className=""
       style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+        width: "400px",
+        height: "400px",
+      }}
+    >
+      <Tabs.Root defaultValue="1" className="flex flex-col">
+        <Tabs.List aria-label="笔记同步" className="shrink-0 border-b border-jaguar-800">
+          <Tabs.Trigger value="1">状态</Tabs.Trigger>
+          <Tabs.Trigger value="2">记录</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="1" className="flex-1">Tab 1 content</Tabs.Content>
+        <Tabs.Content value="2" className="flex-1">Tab 2 content</Tabs.Content>
+      </Tabs.Root>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      'jaguar': {
+        '50': '#eae7ff',
+        '100': '#d8d3ff',
+        '200': '#bcb0ff',
+        '300': '#9780ff',
+        '400': '#814fff',
+        '500': '#7727ff',
+        '600': '#7903ff',
+        '700': '#7500ff',
+        '800': '#6000d3',
+        '900': '#4e0ba4',
+        '950': '#07010e',
+      },
+
+    }
+  },
+  plugins: [],
+}
+
